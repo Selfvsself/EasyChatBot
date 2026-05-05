0b583ae24293e86725a8a30839cfe2e241661e10
Selfvsself <killerman323@gmail.com>
2026-05-05 16:16:31 +0300

    upd agent_pipeline.py

--- handlers/pipelines/agent_pipeline.py
+++ handlers/pipelines/agent_pipeline.py
@@ -11,12 +11,13 @@ from handlers.pipelines.steps.search_validations_step import SearchValidationSte
 
 class AgentPipeline(BaseStep):
 
-    def __init__(self, llm_client, max_pages = 5):
+    def __init__(self, llm_client, max_pages = 8, max_attempt = 3):
         super().__init__(llm_client)
         self.max_pages = max_pages
+        self.max_attempt = max_attempt
         self.plan_step = PlanStep(self.llm_client)
         self.answer_step = GenerateStep(self.llm_client)
-        self.web_search = SearchWebStep(self.llm_client, max_results=20)
+        self.web_search = SearchWebStep(self.llm_client, max_results=max_pages * 2)
         self.web_source = SearchPageSourceStep(self.llm_client)
         self.web_summary = SearchSummaryStep(self.llm_client)
         self.web_validation = SearchValidationStep(self.llm_client)
@@ -25,8 +26,7 @@ class AgentPipeline(BaseStep):
         if not context:
             raise ValueError("context is missing or empty")
         last_context = context
-        max_attempts = 3
-        is_validate = False
+        max_attempts = self.max_attempt
         for attempt in range(max_attempts):
             search_plan = await self.plan_step.execute(last_context)
             last_context = search_plan.context
@@ -50,55 +50,44 @@ class AgentPipeline(BaseStep):
                     if actual_source_page:
                         source_pages.extend(actual_source_page)
                         if len(source_pages) >= self.max_pages:
-                            source_ctx = StepContext.from_context(last_context)
-                            source_ctx.search_results = source_pages
-                            await self._emit_stage(stage_callback, "thinking", {})
-                            summary_result = await self.web_summary.execute(source_ctx)
-                            source_pages = []
-                            summary_ctx = summary_result.context
-                            if not summary_ctx:
-                                continue
-                            answer_result = await self.answer_step.execute(summary_ctx)
-                            answer_ctx = answer_result.context
-                            await self._emit_stage(stage_callback, "web_validating", {})
-                            valid_result = await self.web_validation.execute(answer_ctx)
-                            if valid_result.success or attempt == max_attempts - 1:
-                                last_context = answer_ctx
-                                is_validate = True
-                                break
-                            else:
-                                await self._emit_stage(stage_callback, "thinking", {})
-                                valid_ctx = valid_result.context
-                                next_action = valid_ctx.action
-                                if next_action == ValidationAction.RESPOND:
-                                    answer_second_result = await self.answer_step.execute(summary_result.context)
-                                    answer_second_ctx = answer_second_result.context
-                                    valid_second_result = await self.web_validation.execute(answer_second_ctx)
-                                    if valid_second_result.success:
-                                        last_context = answer_second_ctx
-                                        is_validate = True
-                                        break
-                                elif next_action == ValidationAction.SEARCH:
-                                    break
-                                elif next_action == ValidationAction.CLARIFY:
-                                    continue
-
-
+                            break
+                source_ctx = StepContext.from_context(last_context)
+                source_ctx.search_results = source_pages
+                await self._emit_stage(stage_callback, "thinking", {})
+                summary_result = await self.web_summary.execute(source_ctx)
+                summary_ctx = summary_result.context
+                if not summary_ctx:
+                    continue
+                answer_result = await self.answer_step.execute(summary_ctx)
+                last_context = answer_result.context
             elif next_step == StepAction.CLARIFY:
                 await self._emit_stage(stage_callback, "thinking", {})
                 result_ctx = StepContext.from_context(last_context)
                 result_ctx.answer = result_ctx.next_step_query
-                last_context = result_ctx
-                is_validate = True
+                return StepResult(context=result_ctx, stop=False)
             else:
                 await self._emit_stage(stage_callback, "thinking", {})
                 result = await self.answer_step.execute(last_context)
                 result_ctx = result.context
                 last_context = result_ctx
-                is_validate = True
 
-            if is_validate:
-                break
+            await self._emit_stage(stage_callback, "web_validating", {})
+            valid_result = await self.web_validation.execute(last_context)
+            if valid_result.success or attempt == max_attempts - 1:
+                return StepResult(context=last_context, stop=False)
+            else:
+                await self._emit_stage(stage_callback, "thinking", {})
+                valid_ctx = valid_result.context
+                next_action = valid_ctx.action
+                if next_action == ValidationAction.RESPOND:
+                    answer_second_result = await self.answer_step.execute(valid_ctx)
+                    answer_second_ctx = answer_second_result.context
+                    valid_second_result = await self.web_validation.execute(answer_second_ctx)
+                    if valid_second_result.success:
+                        last_context = answer_second_ctx
+                        return StepResult(context=last_context, stop=False)
+                else:
+                    continue
 
         return StepResult(context=last_context, stop=False)
 
